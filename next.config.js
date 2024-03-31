c6cc64c1a8b5eaec1f7bfed5f7ad0b4bf761a914
diojer <diogodinosaurus@gmail.com>
2024-03-31 22:56:53 +0100

    production build bugfix

--- next.config.js
+++ next.config.js
@@ -13,6 +13,12 @@ const nextConfig = {
         pathname: "/storage/**",
       },
       {
+        protocol: "https",
+        hostname: "api.leedsthinktank.org.uk",
+        port: "",
+        pathname: "/storage/**",
+      },
+      {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "3000",
