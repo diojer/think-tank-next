6a17afaf4515dfd78ff5a65e1e2886bdc2273093
diojer <diogodinosaurus@gmail.com>
2024-03-31 19:14:16 +0100

    temp bugfix 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,24 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Leeds Think Tank',
-  description: 'Leeds Think Tank (LTT) is a student-led and student-run think tank dedicated to undertaking empirically driven research and non-partisan policy that centres on both local and national issues.',
-}
+  title: "Leeds Think Tank",
+  description:
+    "Leeds Think Tank (LTT) is a student-led and student-run think tank dedicated to undertaking empirically driven research and non-partisan policy that centres on both local and national issues.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
         <body className={inter.className}>
-          <Navbar />
-          <div className='page-wrapper'>
-            {children}
-          </div>
+          <div className="page-wrapper">{children}</div>
         </body>
       </html>
     </>
-  )
+  );
 }
