99d65deca5fcb6464f5ad10298efae5ba3767118
diojer <diogodinosaurus@gmail.com>
2024-04-03 13:07:59 +0100

    middleware bugfix

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,9 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
   ignoredRoutes: ["((?!^/portal).*)"],
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
+  debug: true,
 });
 
 export const config = {
