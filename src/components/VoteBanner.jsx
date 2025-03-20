7f895826e2b5779a5dbb1a604681a85160357e84
diojer <78817584+diojer@users.noreply.github.com>
2025-03-20 18:10:26 +0000

    Update VoteBanner.jsx

--- src/components/VoteBanner.jsx
+++ src/components/VoteBanner.jsx
@@ -4,7 +4,7 @@ import "./VoteBanner.css";
 function VoteBanner() {
   return (
       <div className="votebanner">
-        <p className="voteBannerText">LPI Election nominations will take place soon! <a className="special" href="https://engage.luu.org.uk/election/XY8XY/leeds-policy-institute-202526-committee-election">Click here</a> to participate or to look at available roles.</p>
+        <p className="voteBannerText">LPI Election nominations will take place soon! <a className="special" href="https://engage.luu.org.uk/election/XY8XY/leeds-policy-institute-202526-committee-election">Click here</a> to participate or look at available roles.</p>
         <p className="voteBannerText">Deadline to sign up April 6th.</p>
       </div>
   );
