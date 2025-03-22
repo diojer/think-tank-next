485090f7d2d584c6df38901aa9603db8ea649505
diojer <diogodinosaurus@gmail.com>
2025-03-22 21:04:13 +0000

    added x button

--- src/components/VoteBanner.jsx
+++ src/components/VoteBanner.jsx
@@ -1,12 +1,27 @@
 "use client";
 import "./VoteBanner.css";
+import { useState } from "react";
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark
+} from "@fortawesome/free-solid-svg-icons";
 
 function VoteBanner() {
+  const [open, setStatus] = useState(true);
+  
   return (
+    open && (
       <div className="votebanner">
-        <p className="voteBannerText">LPI Election nominations will take place soon! <a className="special" href="https://engage.luu.org.uk/election/XY8XY/leeds-policy-institute-202526-committee-election">Click here</a> to participate or look at available roles.</p>
-        <p className="voteBannerText">Deadline to sign up April 6th.</p>
+        <div>
+          <p className="voteBannerText">LPI Election nominations will take place soon! <a className="special" href="https://engage.luu.org.uk/election/XY8XY/leeds-policy-institute-202526-committee-election">Click here</a> to participate or look at available roles.</p>
+          <p className="voteBannerText">Deadline to sign up April 6th.</p>
+        </div>
+        <button className="close" type="button" onClick={()=>setStatus(false)}><FontAwesomeIcon icon={faCircleXmark} /></button>
+        
       </div>
+    )
   );
 }
 
