92637845a3c0039a67629e09855d02bae802fe16
diojer <78817584+diojer@users.noreply.github.com>
2025-04-10 13:10:22 +0100

    Update VoteBanner.jsx

--- src/components/VoteBanner.jsx
+++ src/components/VoteBanner.jsx
@@ -15,8 +15,7 @@ function VoteBanner() {
     open && (
       <div className="votebanner">
         <div>
-          <p className="voteBannerText">LPI Election nominations will take place soon! <a className="special" href="https://engage.luu.org.uk/election/XY8XY/leeds-policy-institute-202526-committee-election">Click here</a> to participate or look at available roles.</p>
-          <p className="voteBannerText">Deadline to sign up April 6th.</p>
+          <p className="voteBannerText">LPI Elections happening now! <a className="special" href="https://engage.luu.org.uk/election/XY8XY/leeds-policy-institute-202526-committee-election">Click here</a> to see the candidates and vote.</p>
         </div>
         <button className="close" type="button" onClick={()=>setStatus(false)}><FontAwesomeIcon icon={faCircleXmark} /></button>
         
